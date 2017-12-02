ffb52ba700b4c394024efd1c7db1ca75493abd6f
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2017-12-02 14:31:01 +0100

    Day 01 part 2 solved

--- src/main/scala/org/lafeuille/adventofcode/y2017/d01/Day01.scala
+++ src/main/scala/org/lafeuille/adventofcode/y2017/d01/Day01.scala
@@ -1,6 +1,7 @@
 package org.lafeuille.adventofcode.y2017.d01
 
 import org.lafeuille.adventofcode.y2017.d01.Day01.{myInput, numbers}
+import org.lafeuille.adventofcode.y2017.d01.Day01Part1.compute
 
 import scala.io.Source
 import scala.io.Source.fromURL
@@ -13,16 +14,32 @@ object Day01 {
   def numbers(input: Source): List[Int] =
     input.toList.map(_.toString).map(_.toInt)
 
-}
-
-object Day01Part1 extends App {
-
-  def compute(numbers: List[Int]): Int = {
-    numbers.zip(numbers.tail ::: numbers.head :: Nil)
+  def compute(numbers: List[Int])(zipFunction: List[Int] => List[Int]): Int = {
+    numbers.zip(zipFunction(numbers))
       .filter { case (x, y) => x == y }
       .map { case (x, _) => x }
       .sum
   }
+
+}
+
+object Day01Part1 extends App {
+
+  def compute(numbers: List[Int]): Int =
+    Day01.compute(numbers) { list =>
+      list.tail ::: list.head :: Nil
+    }
+
+  println(compute(numbers(myInput)))
+}
+
+object Day01Part2 extends App {
+
+  def compute(numbers: List[Int]): Int =
+    Day01.compute(numbers) { list =>
+      val (start, end) = list.splitAt(list.size / 2)
+      end ::: start
+    }
 
   println(compute(numbers(myInput)))
 }
--- src/test/scala/org/lafeuille/adventofcode/y2017/d01/Day01Part2Spec.scala
+++ src/test/scala/org/lafeuille/adventofcode/y2017/d01/Day01Part2Spec.scala
@@ -4,30 +4,31 @@ import org.lafeuille.adventofcode.y2017.d01.Day01.{myInput, numbers}
 import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.{Matchers, WordSpec}
 
-class Day01Part1Spec extends WordSpec with Matchers with TableDrivenPropertyChecks {
+class Day01Part2Spec extends WordSpec with Matchers with TableDrivenPropertyChecks {
 
   "Sample input" should {
 
     val samples = Table(
       ("Numbers", "Sum"),
-      (List(1, 1, 2, 2), 3),
-      (List(1, 1, 1, 1), 4),
-      (List(1, 2, 3, 4), 0),
-      (List(9, 1, 2, 1, 2, 1, 2, 9), 9)
+      (List(1, 2, 1, 2), 6),
+      (List(1, 2, 2, 1), 0),
+      (List(1, 2, 3, 4, 2, 5), 4),
+      (List(1, 2, 3, 1, 2, 3), 12),
+      (List(1, 2, 1, 3, 1, 4, 1, 5), 4),
     )
 
     forAll(samples) { (numbers, sum) =>
       s"have $numbers sum be $sum" in {
-        Day01Part1.compute(numbers) shouldBe sum
+        Day01Part2.compute(numbers) shouldBe sum
       }
     }
   }
 
   "My input" should {
-    val myResult = 1034
+    val myResult = 1356
 
     s"have sum be $myResult" in {
-      Day01Part1.compute(numbers(myInput)) shouldBe myResult
+      Day01Part2.compute(numbers(myInput)) shouldBe myResult
     }
   }
 
