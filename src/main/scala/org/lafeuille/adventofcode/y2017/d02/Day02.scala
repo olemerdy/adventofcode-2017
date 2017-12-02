4ce41c3f3b1531b1e918eddf5dca60e85c25df4e
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2017-12-02 11:15:31 +0100

    Day 2 part 2

--- src/main/scala/org/lafeuille/adventofcode/y2017/d02/Day02.scala
+++ src/main/scala/org/lafeuille/adventofcode/y2017/d02/Day02.scala
@@ -1,23 +1,52 @@
 package org.lafeuille.adventofcode.y2017.d02
 
-import org.lafeuille.adventofcode.y2017.d02.Day02.{myInput, spreadsheet}
+import org.lafeuille.adventofcode.y2017.d02.Day02.{Row, Spreadsheet, myInput, spreadsheet}
 
+import scala.io.Source
 import scala.io.Source.fromURL
 
 object Day02 {
 
-  def myInput: Iterator[String] =
-    fromURL(getClass.getResource("input.txt")).getLines()
+  type Row = List[Int]
+  type Spreadsheet = List[Row]
 
-  def spreadsheet(input: Iterator[String]): Seq[Seq[Int]] =
-    input.map(_.split("\\t").map(_.toInt).toSeq).toSeq
+  def myInput: Source =
+    fromURL(getClass.getResource("input.txt"))
+
+  def spreadsheet(input: Source): Spreadsheet =
+    input.getLines.map(_.split("\\t").map(_.toInt).toList).toList
 
 }
 
 object Day02Part1 extends App {
 
-  def checksum(spreadsheet: Seq[Seq[Int]]) =
-    spreadsheet.map(row => row.max - row.min).sum
+  def rowChecksum(row: Row) =
+    row.max - row.min
+
+  def checksum(spreadsheet: Spreadsheet) =
+    spreadsheet.map(row => rowChecksum(row)).sum
+
+  println(checksum(spreadsheet(myInput)))
+}
+
+object Day02Part2 extends App {
+
+  def rowChecksum(row: Row): Int =
+    row.combinations(2)
+      .map {
+        case x :: y :: _ if x < y => (x, y)
+        case x :: y :: _ => (y, x)
+      }
+      .find {
+        case (x, y) => y % x == 0
+      }
+      .map {
+        case (x, y) => y / x
+      }
+      .getOrElse(0)
+
+  def checksum(spreadsheet: Spreadsheet): Int =
+    spreadsheet.map(row => rowChecksum(row)).sum
 
   println(checksum(spreadsheet(myInput)))
 }
--- src/test/scala/org/lafeuille/adventofcode/y2017/d02/Day02Spec.scala
+++ src/test/scala/org/lafeuille/adventofcode/y2017/d02/Day02Spec.scala
@@ -1,20 +1,83 @@
 package org.lafeuille.adventofcode.y2017.d02
 
 import org.lafeuille.adventofcode.y2017.d02.Day02.{myInput, spreadsheet}
+import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.{Matchers, WordSpec}
 
-class Day02Part1Spec extends WordSpec with Matchers {
-
-  lazy val myResult = 32121
+class Day02Part1Spec extends WordSpec with Matchers with TableDrivenPropertyChecks {
 
   "My input" should {
+    val myResult = 32121
+
     s"have checksum $myResult" in {
       Day02Part1.checksum(spreadsheet(myInput)) shouldBe myResult
     }
   }
 
+  "Sample input" should {
+    val row1 = List(5, 1, 9, 5)
+    val row2 = List(7, 5, 3)
+    val row3 = List(2, 4, 6, 8)
+
+    val rows = Table(
+      ("Index", "Row", "Checksum"),
+      (1, row1, 8),
+      (2, row2, 4),
+      (3, row3, 6),
+    )
+
+    forAll(rows) { (index, row, checksum) =>
+      s"have row $index checksum be $checksum" in {
+        Day02Part1.rowChecksum(row) shouldBe checksum
+      }
+    }
+
+    val spreadsheet = row1 :: row2 :: row3 :: Nil
+    val result = 18
+
+    s"have checksum $result" in {
+      Day02Part1.checksum(spreadsheet) shouldBe result
+    }
+  }
+
 }
 
-class Day02Part2Spec extends WordSpec with Matchers {
+class Day02Part2Spec extends WordSpec with Matchers with TableDrivenPropertyChecks {
+
+
+  "My input" should {
+    val myResult = 197
+
+    s"have checksum $myResult" in {
+      Day02Part2.checksum(spreadsheet(myInput)) shouldBe myResult
+    }
+  }
+
+  "Sample input" should {
+
+    val row1 = List(5, 9, 2, 8)
+    val row2 = List(9, 4, 7, 3)
+    val row3 = List(3, 8, 6, 5)
+
+    val rows = Table(
+      ("Index", "Row", "Checksum"),
+      (1, row1, 4),
+      (2, row2, 3),
+      (3, row3, 2),
+    )
+
+    forAll(rows) { (index, row, checksum) =>
+      s"have row $index checksum be $checksum" in {
+        Day02Part2.rowChecksum(row) shouldBe checksum
+      }
+    }
+
+    val spreadsheet = row1 :: row2 :: row3 :: Nil
+    val result = 9
+
+    s"have checksum be $result" in {
+      Day02Part2.checksum(spreadsheet) shouldBe result
+    }
+  }
 
 }
